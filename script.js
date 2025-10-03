eb77f3cf534f7021e9d042376e48b263d9eb2c89
Асхат <106676539+A100R@users.noreply.github.com>
2025-10-03 13:00:45 +0300

    Create script.js
    changed

---
+++ script.js
@@ -0,0 +1,8 @@
+{function(){
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementBuId('button').addEventListener('click'. function(){
+      let text = document.getElementById('text').value;
+      alert('Длина тексат: ' + text.length};
+    });
+  });
+})();
