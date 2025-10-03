931de2a8a7d5b500fdb1af1c71fef36c91e9567d
Асхат <106676539+A100R@users.noreply.github.com>
2025-10-03 13:30:16 +0300

    Update script.js
    corrected

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   document.addEventListener('DOMContentLoaded', function(){
     document.getElementBuId('button').addEventListener('click'. function(){
       let text = document.getElementById('text').value;
-      alert('Длина тексат: ' + text.length};
+      alert('Длина текста: ' + text.length};
     });
   });
 })();
